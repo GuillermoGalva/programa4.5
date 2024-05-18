f7f5f1069d3b8a2d49aa24417ea4c090a5b1705a
guill <guill@GUILLERMO-PC>
2024-05-17 21:19:17 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ingresa la cantidad de piezas a procesar:");
+        int n = int.Parse(Console.ReadLine());
+
+        int contador = 0;
+        int contadorAptas = 0;
+
+        while (contador < n)
+        {
+            Console.Write($"Ingresa la longitud de la pieza {contador + 1}: ");
+            double longitud = double.Parse(Console.ReadLine());
+
+            if (longitud >= 1.20 && longitud <= 1.30)
+            {
+                contadorAptas++;
+            }
+
+            contador++;
+        }
+
+        Console.WriteLine($"La cantidad de piezas aptas es: {contadorAptas}");
+    }
+}
+
